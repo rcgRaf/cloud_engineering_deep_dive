6369f871ce95989f174a8b1968a999bbb9db525d
rcgRaf <rubomaf@gmail.com>
2024-12-03 02:21:40 +0400

    upadate code

--- src/lambda/src/index.js
+++ src/lambda/src/index.js
@@ -2,6 +2,6 @@ exports.handler = async (event) => {
     console.log("Hello World");
     return {
         statusCode: 200,
-        body: JSON.stringify('Hello World!'),
+        body: JSON.stringify('Hello World from V2!'),
     };
 };
